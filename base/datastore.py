5ca332a6522ec5467e97fef6f89cfcad5dc6b861
jekabs <jekabs@gritans.lv>
2022-02-23 15:39:47 +0200

    fixed job queuing mechanism and threading

--- base/datastore.py
+++ base/datastore.py
@@ -1,5 +1,6 @@
 from threading import Lock
 from typing import List
+from time import sleep
 
 class DataStore:
     def __init__(self):
@@ -7,11 +8,23 @@ class DataStore:
         self.items = []
         self.empty = True
     
+    def add(self, item):
+        with self.locl:
+            self.items.append(item)
+            self.empty = False
+
     def add_many(self, items):
         with self.lock:
             self.items.extend(items)
             self.empty = False
     
+    def get_one(slef) -> object:
+        with self.lock:
+            item = self.items.pop()
+            if len(self.items) == 0:
+                self.empty = True
+        return item
+
     def get_all(self) -> List[object]:
         with self.lock:
             items, self.items = self.items, []
--- base/scraper.py
+++ base/scraper.py
@@ -1,8 +1,9 @@
 from base.db import DataBase
 from .target import Target
 from concurrent.futures import ThreadPoolExecutor
-from time import sleep
+from time import sleep, time
 from threading import Thread
+from itertools import islice
 from requests.exceptions import ProxyError
 from .proxies import Proxy
 from .headers import Headers
@@ -10,32 +11,59 @@ from .user import User
 from .config import CONCURRENT_REQUESTS
 from .datastore import DataStore
 
-
 class DataConsumer(Thread):
-    def __init__(self, data_store: DataStore, data_base: DataBase, db_table: str):
+    def __init__(self, data_store: DataStore, db_table: Table):
         Thread.__init__(self)
         self.data_store = data_store
-        self.db = data_base
         self.db_table = db_table
 
     def run(self):
         while True:
             if not self.data_store.empty:
                 items = self.data_store.get_all()
-                self.db.insert(self.db_table, items)
-            print("Batch uploaded to DB")
+                self.db_table.insert_many(items)
+            print("Batch inserted")
             sleep(3)
 
 
 class Scraper:
     """Handles threading for web scraping"""
-    def __init__(self, target: Target):
+    def __init__(self, target: Target, user_master: UserMaster):
         self.target = target
+        self.user_master = user_master
         self.data_store = DataStore()
-        self.data_consumer = DataConsumer(self.data_store, self.target.db, self.target.table)
+        self.data_consumer = DataConsumer(self.data_store, self.target.table)
+        self._url_generator = self.target.urls()
+        self.url_queue = DataStore()
+    
+    def populate_queue(self):
+        """Populates URLs to scrape from the target URL generator"""
+        urls = islice(self._url_generator, 100)#TODO: make N configurable
+        self.url_queue.add_many(urls)
+        return bool(urls)
+
+    def scrape_target(self, url)
+     """Scrape a single url"""
+        user = self.user_master.designee()
+        try:
+            r = requests.get(url, proxies=user.proxy.dict(), headers=user.headers.dict())
+        except requests.exceptions.ProxyError:
+            self.url_queue.add(url)
+
+        if r.status_code == 200: # OK response
+            items, urls_to_scrape = self.target.extract_items(r)
+            self.data_store.add_many(items)
+            self.url_queue.add_many(urls_to_scrape)
+
+        elif r.status_code == 429: # Bad repsonse, usually proxy detected - myb pause scraping?
+            ...
+        else:#TODO: if non-fatal status_code, add url back to queue
+            ...
 
     def start(self):
-        args = ((url, User.from_randagent_proxyrotator(), self.data_store) for url in self.target._url_gen)
         self.data_consumer.start()
-        ThreadPoolExecutor(max_workers=CONCURRENT_REQUESTS).map(lambda ar: self.target.scrape(*ar), args)         
-                    
+        while True and self.:
+            if not self.populate_queue():
+                break
+            ThreadPoolExecutor(max_workers=CONCURRENT_REQUESTS).map(self.scrape_target, self.url_queue.get_all())
+        print("Finished")
