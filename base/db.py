0522d35f6b48206643b897006816dd29eda0cd87
jekabs <jekabs@gritans.lv>
2022-02-23 15:45:33 +0200

    necessary abstract methods for db

--- base/db.py
+++ base/db.py
@@ -1,44 +1,17 @@
-import psycopg2
-from typing import NamedTuple, List
-from .config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
-from datetime import datetime
-
-def sqlise(val):
-    """
-    Convert values to be SQL query friendly
-    """
-    if isinstance(val,str):
-        return f"""
-        '{val.replace("'","''")}'
-        """
-    
-    if isinstance(val,int):
-        return str(val)
-
-    if isinstance(val, datetime):
-        return val.strftime("'%Y-%m-%d %H:%M:%S-00'")
-
 
 class DataBase:
-    """One instance handles communication with one database"""
-    def __init__(self, host = DB_HOST, port = DB_PORT, name = DB_NAME, user = DB_USER, password = DB_PASSWORD):
-        self._conn = psycopg2.connect(host=host, port=port, dbname=name, user=user, password=password)
-        self.cursor = self._conn.cursor()
+    """Abstracts a database instance"""
     
-    def insert(self, table: str, rows: List[NamedTuple]):
-        """Inserts NamedTuples into a table, where each field of the NamedTuple is a column"""
-        sql_values = []
-        for row in rows:
-            field_string = "("+",".join(row._fields)+")"
-            values_string_list = [sqlise(value) for value in row]
-            values_string = "("+",".join(values_string_list)+")"
-            sql_values.append(values_string)
+    def connect(self):
+        """Establishes a connection with the database"""
 
-        sql_values_string = ",".join(sql_values).rstrip()               
 
-        self.cursor.execute(
-            f"""INSERT INTO {table} {field_string} VALUES {sql_values_string}"""
-        )
-        self._conn.commit()
+class Table:
+    """Abstracts a table within a database instance"""
+    db: DataBase
+    
+    def insert(self, item):
+        """Inserts a single item in the database table"""
 
-local_db = DataBase()#default params are params from config.py
+    def insert_many(self, items):
+        """Inserts multiple items in the database table"""
